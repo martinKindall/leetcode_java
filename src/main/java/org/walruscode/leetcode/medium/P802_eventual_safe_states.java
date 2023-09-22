9d83b6431e6192a50acde81ec6f6e885b0989871
Martin Cornejo <mart256@gmail.com>
2023-09-22 23:11:31 +0200

    Updated solution

--- src/main/java/org/walruscode/leetcode/medium/P802_eventual_safe_states.java
+++ src/main/java/org/walruscode/leetcode/medium/P802_eventual_safe_states.java
@@ -5,6 +5,76 @@ import java.util.*;
 public class P802_eventual_safe_states {
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+
+        boolean[] isBad = new boolean[n];
+        boolean[] isSafe = new boolean[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (!isBad[i] && !isSafe[i]) {
+                dfs(isBad, isSafe, graph, i);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            if (isSafe[i])
+                result.add(i);
+        return result;
+    }
+
+    private boolean dfs(boolean[] isBad, boolean[] isSafe, int[][] graph, int current) {
+        isBad[current] = true;
+
+        for (int neighbor: graph[current]) {
+            if (!isSafe[neighbor] && (isBad[neighbor] || !dfs(isBad, isSafe, graph, neighbor)))
+                return false;
+        }
+
+        isBad[current] = false;
+        isSafe[current] = true;
+
+        return true;
+    }
+
+    public List<Integer> eventualSafeNodesKahn(int[][] graph) {
+        int[] outDegrees = new int[graph.length];
+        List<Integer>[] inverted = new List[graph.length];
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < graph.length; i++) {
+            outDegrees[i] = graph[i].length;
+            inverted[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < graph.length; i++) {
+            for (int neighbor: graph[i]) {
+                inverted[neighbor].add(i);
+            }
+
+            if (outDegrees[i] == 0) queue.offer(i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        while (!queue.isEmpty()) {
+            Integer current = queue.poll();
+            result.add(current);
+
+            for (Integer neighbor: inverted[current]) {
+
+                if (--outDegrees[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        Collections.sort(result);
+
+        return result;
+    }
+
+    public List<Integer> eventualSafeNodesDfsSlow(int[][] graph) {
 
         int[] inDegrees = new int[graph.length];
         int[] outDegrees = new int[graph.length];
@@ -32,7 +102,7 @@ public class P802_eventual_safe_states {
         for (int i = 0; i < graph.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                dfs(i, graph, visited, result);
+                dfsOld(i, graph, visited, result);
             }
         }
 
@@ -42,30 +112,23 @@ public class P802_eventual_safe_states {
         return resultSort;
     }
 
-    private boolean dfs(int current, int[][] graph, boolean[] visited, Set<Integer> result) {
-
-        int countSafe = 0;
+    private boolean dfsOld(int current, int[][] graph, boolean[] visited, Set<Integer> result) {
 
         for (int neighbor: graph[current]) {
-            if (result.contains(neighbor)) countSafe++;
+            if (result.contains(neighbor)) continue;
             else if (visited[neighbor]) {
-                break;
+                return false;
             }
             else {
                 visited[neighbor] = true;
-                boolean isSafe = dfs(neighbor, graph, visited, result);
+                boolean isSafe = dfsOld(neighbor, graph, visited, result);
 
-                if (isSafe) countSafe++;
-                else break;
+                if (!isSafe) return false;
             }
         }
 
-        if (countSafe == graph[current].length) {
-            result.add(current);
+        result.add(current);
 
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
